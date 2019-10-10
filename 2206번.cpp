106e9db5b49153e62344dbcb57d9c26f2cae34de
mingg123 <s_p_g_b_@nate.com>
2019-10-11 00:11:36 +0900

    2206 BOJ

--- 2206번.cpp
+++ 2206번.cpp
@@ -2,41 +2,42 @@
 #include <queue>
 using namespace std;
 int Graph[1001][1001];
+int direct[4][2] = { {1,0},{0,1},{-1,0},{0,-1} };
+int visitied[1001][1001][2];
 int N, M;
-int visited[1001][1001];
-int direct[4][2] = { {0,1},{0,-1},{1,0},{-1,0} };
 struct wall {
-	int x, y, wall;
+	int x, y, wall1;
 };
 queue <wall> q;
-void BFS(int startx, int starty, int n, int m)
+void BFS()
 {
-	q.push({ startx, starty,1 });
-	visited[startx][starty] = 1;
+	q.push({ 1,1,0 });
+	visitied[1][1][0] = 1;
 	while (!q.empty())
 	{
-		int frontx = q.front().x;
-		int fronty = q.front().y;
-		int wall = q.front().wall;
-		if (frontx == n && fronty == m)
+		int x = q.front().x;
+		int y = q.front().y;
+		int wall = q.front().wall1;
+		q.pop();
+		if (x == N && y == M)
 		{
-			printf("%d", visited[frontx][fronty] );
+			cout << visitied[N][M][wall] << endl;
 			return;
 		}
-		q.pop();
 		for (int i = 0; i < 4; i++)
 		{
-			int nextx = frontx + direct[i][0];
-			int nexty = fronty + direct[i][1];
-			if (nextx >= 1 && nextx <= n && nexty >= 1 && nexty <= m && Graph[nextx][nexty] == 1 && wall == 1)
+			int nextx = x + direct[i][0];
+			int nexty = y + direct[i][1];
+
+			if (Graph[nextx][nexty] == 1 && wall == 0&& nextx>=1 && nexty>=1 && nextx<=N && nexty<=M)
 			{
-				q.push({ nextx, nexty, wall - 1 });
-				visited[nextx][nexty] = visited[frontx][fronty] + 1;
+				q.push({ nextx, nexty, wall + 1 });
+				visitied[nextx][nexty][wall+1] = visitied[x][y][wall] + 1;
 			}
-			else if (nextx >= 1 && nextx <= n && nexty >= 1 && nexty <= m && Graph[nextx][nexty] == 0 && visited[nextx][nexty] == 0)
+			else if (Graph[nextx][nexty] == 0 && nextx >= 1 && nexty >= 1 && nextx <= N && nexty <= M && visitied[nextx][nexty][wall]==0)
 			{
-				q.push({ nextx, nexty, wall });
-				visited[nextx][nexty] = visited[frontx][fronty] + 1;
+			q.push({ nextx, nexty, wall });
+			visitied[nextx][nexty][wall] = visitied[x][y][wall] + 1;
 			}
 		}
 	}
@@ -51,15 +52,29 @@ int main()
 		{
 			int a;
 			scanf_s("%1d", &a);
-			Graph[i][j] = a;
+			Graph[i][j] = a; 
 		}
 	}
+	BFS();
+	
 	for (int i = 1; i <= N; i++)
 	{
 		for (int j = 1; j <= M; j++)
 		{
-			//printf("%d", Graph[i][j]);
+			for (int k = 0; k <= 1; k++)
+			{
+				cout << visitied[i][j][k] << " ";
+			}
 		}
+		cout << endl;
 	}
-	BFS(1, 1, N, M);
+	/*
+	if (Graph[N][M] != 0)
+	{
+		cout << Graph[N][M] << endl;
+	}
+	else {
+		cout << "-1" << endl;
+	}
+	*/
 }
